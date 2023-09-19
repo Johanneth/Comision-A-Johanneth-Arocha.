9e6a5f59e4154af28cbdecb34fcedf3c4aba019a
Johanneth <jaagav4@gmail.com>
2023-09-19 12:34:28 -0300

    primer commit, se configura el entorno y se crean las rutas

---
+++ src/controllers/task.controllers.js
@@ -0,0 +1,24 @@
+//controlador para traer todas las tareas
+export const ctrlGetTask = (req, res) => {
+
+}
+
+
+//controlador para crear todas las tareas
+const ctrlGetTask = (req, res) => {
+
+
+}
+
+
+//controlador para modificar una tarea
+const ctrlCreateTask = (req, res) => {
+
+
+}
+
+//controlador para crear una tarea
+const ctrlDeleteTask = (req, res) => {
+
+
+}
---
+++ src/routes/task.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express" ;
+
+const taskRouter = Router ();
+
+//endpoint para traer todas las tareas
+taskRouter.get('/api/tasks',() => {
+
+})
+
+//endpoint para crear todas las tareas
+taskRouter.post('/api/tasks',() => {
+
+})
+
+//endpoint para modificar todas las tareas
+taskRouter.put('/api/:id' )
+
+//endpoint para eliminar todas las tareas
+taskRouter.delete('/api/:id' )
+
